4a6fdf5fffd59c9be85d6af57b46add02b80f502
Bruno Brian Sousa <bruninho_brian@hotmail.com>
2021-10-13 18:11:30 -0300

    creating lessons and contents modules

---
+++ src/modules/contents/dto/content.dto.ts
@@ -0,0 +1,13 @@
+import { FilterableField } from '@nestjs-query/query-graphql';
+import { ObjectType } from '@nestjs/graphql';
+
+import { BaseDTO } from 'src/modules/bases/dto/base.dto';
+
+@ObjectType('Content')
+export class ContentDTO extends BaseDTO {
+  @FilterableField()
+  description: string;
+
+  @FilterableField()
+  linkContent: string;
+}
---
+++ src/modules/lessons/dto/lesson.dto.ts
@@ -0,0 +1,10 @@
+import { FilterableField } from '@nestjs-query/query-graphql';
+import { ObjectType } from '@nestjs/graphql';
+
+import { BaseDTO } from 'src/modules/bases/dto/base.dto';
+
+@ObjectType('Lesson')
+export class LessonDTO extends BaseDTO {
+  @FilterableField()
+  description: string;
+}
